223c8224fc5c0f7ae858499151d478ce5c324203
Edyta2801 <edyta.szarowska@interia.pl>
2022-03-30 09:45:26 +0200

    refactor Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../Footer";
 import MainNavigation from "../MainNavigation";
+import { styled } from '@mui/material/styles';
 
 interface Props {
   children?: React.ReactNode;
@@ -8,14 +9,22 @@ interface Props {
 
 function Layout(props: Props) {
   return (
-
-    <div className='all-layout' >
-      <div className='layout'>
+    <AllLayout>
+      <InternalLayout>
         <MainNavigation />
-        <main className='main'>{props.children}</main></div>
-        <Footer/>
-    </div>
+        <main className='main'>{props.children}</main>
+      </InternalLayout>
+      <Footer />
+    </AllLayout>
   );
 }
 
 export default Layout;
+
+const AllLayout = styled('div')(() => ({
+  position: 'relative',
+  minHeight: '100vh'
+}));
+const InternalLayout = styled('div')(() => ({
+  paddingBottom: '5rem'
+}));
