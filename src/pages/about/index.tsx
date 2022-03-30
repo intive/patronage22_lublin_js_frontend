85e6664f5adead56781d37fb41598f042c8372aa
Edyta2801 <edyta.szarowska@interia.pl>
2022-03-30 09:44:56 +0200

    add About Page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,12 @@
+import About from "../../components/About";
+
 function AboutPage() {
   return (
     <section>
       <h3>
         Home <span className="location"> {">"} About</span>
       </h3>
+      <About/>
     </section>
   );
 }
