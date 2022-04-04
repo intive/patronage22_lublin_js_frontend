eaa956176ceaf280d1ee5488208c662794f7dfdf
unknown <dmytro.tarasiuk1899@gmail.com>
2022-04-04 11:57:57 +0200

    add .next to gitignore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/products":["static\u002Fchunks\u002Fpages\u002Fproducts.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fproducts"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
