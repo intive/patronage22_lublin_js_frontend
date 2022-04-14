069be6c06b9269b35a2792e0099a95d181c4ce20
unknown <dmytro.tarasiuk1899@gmail.com>
2022-04-14 11:19:38 +0200

    feat: add redux

---
+++ src/reducers/cartReducer.ts
@@ -0,0 +1,50 @@
+import * as constants from "../types/cartConstants";
+
+export const cartReducer = (
+  state = { cartItems: <any>[], shippingAddress: {} },
+  action: any
+) => {
+  switch (action.type) {
+    case constants.CART_ADD_ITEM:
+      const item = action.payload;
+
+      const existItem: any = state.cartItems.find(
+        (x: any) => x.product === item.product
+      );
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x: any) =>
+            x.product === existItem.product ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+
+    case constants.CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (x: any) => x.product !== action.payload
+        ),
+      };
+    case constants.CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
+      };
+    case constants.CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
